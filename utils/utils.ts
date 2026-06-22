450385bcbead46f2e12847d5bbb938bc0233e9c3
Sheikh Muhtasim Nasif <77929303+Nasif-21@users.noreply.github.com>
2026-06-22 15:36:02 +0600

    Added random number generation function

--- utils/utils.ts
+++ utils/utils.ts
@@ -5,7 +5,6 @@ export function generateRandomNumber(min:number,max:number):number
     const randomNumber=Math.random()*(max-min)+min;
     return Math.floor(randomNumber)
     
-
 }
 
 // Write function to save data in Json file

dc4526c7a2db04ea6689fe88be6c5110d1d34f2a
Nasif <nasifshitab01@gmail.com>
2026-06-28 12:43:03 +0600

    Add logics for save and retrive user login credencials from json file

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,4 +1,5 @@
-//import fs from 'fs';
+import fs from 'fs';
+
 
 export function generateRandomNumber(min:number,max:number):number
 {
@@ -9,12 +10,21 @@ export function generateRandomNumber(min:number,max:number):number
 }
 
 // Write function to save data in Json file
-export function saveJsonData()
+export function saveJsonData(jsonObject:object , filepath:string):void
 {
+    const fileContent=fs.readFileSync(filepath,'utf-8');
+    const jsonArray:object[]=JSON.parse(fileContent);
+    jsonArray.push(jsonObject);
+    fs.writeFileSync(filepath,JSON.stringify(jsonArray),'utf-8');
+
  
 }
 
-export function readJsonData()
+//read latest index from json file
+export function readJsonData(filepath:string):object
 {
+    const fileContent=fs.readFileSync(filepath,'utf-8')
+    const jsonArray:object[]=JSON.parse(fileContent)
+    return jsonArray[jsonArray.length-1];
 
 }

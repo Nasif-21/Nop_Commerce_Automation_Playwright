22bf68d1691f5484d1c65a554aadea12052b1dea
Nasif <nasifshitab01@gmail.com>
2026-07-10 22:16:43 +0600

    Added a state loader for each function for more relaiable test under low connectivity

--- pages/login.ts
+++ pages/login.ts
@@ -9,26 +9,34 @@ export class LoginPage
     {
         await this.page.getByLabel("Email").fill(email);
         await this.page.getByLabel("Password").fill(password);
+        await this.page.waitForLoadState();
         await this.page.getByRole("button",{name:"LOG IN"}).click();
     }
 
     async doEmptyLogin()
     {
+      await this.page.waitForLoadState();
      await this.page.getByRole("button",{name:"LOG IN"}).click();
+     
     }
 
-    async doOneFieldInvalidLogin(email:string)
+    async doOneFieldvalidLogin(email:string)
     {
+        await this.page.waitForLoadState();
         await this.page.getByLabel("Email").fill(email);
         await this.page.getByRole("button",{name:"LOG IN"}).click();
+        
+
 
     }
 
     async doAllInvalidLogin(email:string,password:string)
     {
+        await this.page.waitForLoadState();
         await this.page.getByLabel("Email").fill(email);
         await this.page.getByLabel("Password").fill(password);
         await this.page.getByRole("button",{name:"LOG IN"}).click();
+        
 
     }
 
